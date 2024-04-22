0b32734ddeae2b28633a20e8accb0cfc10b595ab
pradip <pradip.kharal777@gmail.com>
2024-04-23 02:36:51 +0545

    template update

--- src/templates/db/postgresql.ts
+++ src/templates/db/postgresql.ts
@@ -4,12 +4,7 @@ export const FOREIGN_KEY_TEMPLATE =
 export const UNIQUE_INDEX_TEMPLATE = '{{INDEX_VALUE}};'
 export const DEFAULT_TEMPLATE = 'DEFAULT {{DEFAULT_VALUE}}'
 export const TABLE_COL_TEMPLATE = '"{{COL_NAME}}" {{COL_TYPE}} {{NULLABLE}} {{DEFAULT_VALUE}}'
-export const CREATE_TABLE_QUERY = `/*
-   Created by pradip kharal 
-   https://github.com/92P96AK 
-   */
-
-   {{CREATE_ENUM_QUERY}}
+export const CREATE_TABLE_QUERY = `{{CREATE_ENUM_QUERY}}
 -- Create table {{TABLE}} 
 
 CREATE TABLE IF NOT EXISTS {{SCHEMA}}."{{TABLE}}" (
