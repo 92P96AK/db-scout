1a8000eeeb3a60a321d63fa9e55a787eed0e3594
pradip <pradip.kharal777@gmail.com>
2024-04-26 02:26:15 +0545

    gen doc, gen interface

--- src/lib/scout_postgresql.ts
+++ src/lib/scout_postgresql.ts
@@ -1,39 +1,56 @@
-import { IEnumRes, IPostgresRes, IPostgresTableRes, ImigrationRes } from '../interface'
-import { DB_INFO_QUERY, ENUM_INFO_QUERY } from '../queries'
-import { GLOBAL, PUBLIC, SCHEMA_TYPE_TEMPLATE } from '../constants'
+import { DbUrlProps, IDbInfoRes, IPostgresResDBS, ImigrationRes } from '../interface'
+import { DB_INFO_QUERY } from '../queries'
+import { GLOBAL, PUBLIC, PostgresDataTypesAndOrm, SCHEMA_TYPE_TEMPLATE } from '../constants'
 import { PostgresqlClient } from '../clients'
 import { MigrationOrder, ParseQueryTemplate } from '../utils'
 
 export class ScoutPostgresqlDb {
-  private client: PostgresqlClient
+  private sourceClient: PostgresqlClient
+  private destinationClient: PostgresqlClient
   private migrationOrderClass: MigrationOrder
-  constructor(dbUrl: string) {
-    this.client = new PostgresqlClient(dbUrl)
+  constructor(props: DbUrlProps) {
+    this.sourceClient = new PostgresqlClient(props.sourceDbUrl)
+    if (props?.destinationDbUrl) {
+      this.destinationClient = new PostgresqlClient(props.destinationDbUrl)
+    }
     this.migrationOrderClass = new MigrationOrder()
   }
-  private getDatabaseInfo(): Promise<IPostgresRes> {
+
+  public getDatabaseInfo(): Promise<IPostgresResDBS> {
     return new Promise(async (resolve, reject) => {
       try {
-        const tables: Array<IPostgresTableRes> = await this.client.query(
-          DB_INFO_QUERY.replace(new RegExp(`${SCHEMA_TYPE_TEMPLATE}`, GLOBAL), this.client.parsedUrl.schema || PUBLIC),
+        const sourceDBInfo: Array<IDbInfoRes> = await this.sourceClient.query(
+          DB_INFO_QUERY.replace(
+            new RegExp(`${SCHEMA_TYPE_TEMPLATE}`, GLOBAL),
+            this.sourceClient.parsedUrl.schema || PUBLIC,
+          ),
         )
-        const enums: Array<IEnumRes> = await this.client.query(ENUM_INFO_QUERY)
+        if (this.destinationClient) {
+          const destinationDBInfo: Array<IDbInfoRes> = await this.destinationClient.query(
+            DB_INFO_QUERY.replace(
+              new RegExp(`${SCHEMA_TYPE_TEMPLATE}`, GLOBAL),
+              this.sourceClient.parsedUrl.schema || PUBLIC,
+            ),
+          )
+          resolve({
+            source: sourceDBInfo[0],
+            destination: destinationDBInfo[0],
+          })
+        }
         resolve({
-          database: this.client.parsedUrl.database,
-          schema: this.client.parsedUrl.schema || PUBLIC,
-          tables,
-          enums,
+          source: sourceDBInfo[0],
         })
       } catch (error) {
         reject(error)
       }
     })
   }
+
   getMigrationWithOrder(): Promise<ImigrationRes> {
     return new Promise(async (resolve, reject) => {
       try {
-        const datas = await this.getDatabaseInfo()
-        const circularDependency = await this.migrationOrderClass.checkCircularDependency(datas.tables)
+        const { source, destination } = await this.getDatabaseInfo()
+        const circularDependency = await this.migrationOrderClass.checkCircularDependency(source.tables)
         if (circularDependency.length) {
           throw new Error(
             ` you have circular dependent tables : ${circularDependency
@@ -41,18 +58,167 @@ export class ScoutPostgresqlDb {
               .join('|')}`,
           )
         }
-        const order = await this.migrationOrderClass.getMigrationOrder(datas.tables)
+        // if (!destination) {
+        const order = await this.migrationOrderClass.getMigrationOrder(source.tables)
         const queries = await new ParseQueryTemplate({
-          database: datas.database,
-          schema: datas.schema,
           tables: order,
-          enums: datas.enums
+          enums: source.enums,
         }).getMigrattion()
         resolve({
           ...queries,
-          migrationOrder: order.map((d) => d.name),
-          isCircularDependent: false,
         })
+        // } else {
+        //   const enums: Array<any> = []
+        //   const tables: Array<any> = []
+        //   source.enums.forEach((se) => {
+        //     const de = destination.enums.find((de) => de.name === se.name)
+        //     if (!de) {
+        //       enums.push({ ...se, isnew: true, haschanged: false, newen: [], dropen: [] })
+        //     } else {
+        //       const newen = se.values.filter((ss) => !de.values.includes(ss))
+        //       const dropen = de.values.filter((dd) => !se.values.includes(dd))
+        //       enums.push({ ...se, isnew: false, haschanged: !!newen?.length || !!dropen?.length, newen, dropen })
+        //     }
+        //   })
+
+        //   // WARN :: if table name is changed or col name is changed it will treat changed as new and old as dropped so data gets lost
+
+        //   source.tables.forEach((st) => {
+        //     const dest = destination.tables.find((dt) => dt.name === st.name)
+        //     if (!dest) {
+        //       tables.push({
+        //         ...st,
+        //         isnew: true,
+        //         haschanged: false,
+        //         newcol: [],
+        //         dropcol: [],
+        //         // changedcol: [],
+        //       })
+        //     } else {
+        //       const newcol = st.cols.filter((ss) => !st.cols.map((f) => f.column_name).includes(ss.column_name))
+        //       const dropcol = dest.cols.filter((ss) => !dest.cols.map((f) => f.column_name).includes(ss.column_name))
+        //       // const changedcol = dest.cols.filter((ss) => !dest.cols.map((f) => f.column_name).includes(ss.column_name))
+        //       if (newcol?.length || dropcol?.length) {
+        //         tables.push({
+        //           ...st,
+        //           isnew: false,
+        //           haschanged: !!newcol?.length || !!dropcol?.length,
+        //           dropcol,
+        //           // changedcol,
+        //         })
+        //       }
+        //     }
+        //   })
+        // }
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  public getDatabaseDocumentation(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const sourceDBInfo: Array<IDbInfoRes> = await this.sourceClient.query(
+          DB_INFO_QUERY.replace(
+            new RegExp(`${SCHEMA_TYPE_TEMPLATE}`, GLOBAL),
+            this.sourceClient.parsedUrl.schema || PUBLIC,
+          ),
+        )
+        let doc = `
+       
+       //     Created by pradip kharal 
+       //     https://github.com/92P96AK 
+       `
+
+        sourceDBInfo[0]?.enums.forEach((en) => {
+          doc += `
+        Data type : Enum
+        Name : ${en.name}
+        Values : (${en.values.join(',')})
+        `
+        })
+        sourceDBInfo[0]?.tables.forEach((table) => {
+          doc += `
+        Table Name : ${table.name}
+
+        Columns : (
+          ${table.cols
+            ?.map((col) => {
+              return `name : ${col.column_name}    type :${col.udt_name}    null? :${!!col.is_nullable}   default? :${
+                col.column_default
+              }    `
+            })
+            .join(' \n')}
+        )
+
+        Primary Keys : (
+             ${table.p_keys?.map((pk) => `${pk.column_name}`)}
+        )
+
+        Foreign Keys : (
+          ${table.f_keys?.map((fk) => `${fk.constraint_values?.split('REFERENCES ')[1]?.split(' ')[0]}`)}
+        )
+        `
+        })
+
+        resolve(doc)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  public getInterfaces(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const sourceDBInfo: Array<IDbInfoRes> = await this.sourceClient.query(
+          DB_INFO_QUERY.replace(
+            new RegExp(`${SCHEMA_TYPE_TEMPLATE}`, GLOBAL),
+            this.sourceClient.parsedUrl.schema || PUBLIC,
+          ),
+        )
+        let doc = `
+       
+       //     Created by pradip kharal 
+       //     https://github.com/92P96AK 
+
+       ${
+         sourceDBInfo[0]?.enums?.length > 0
+           ? `
+
+       ${sourceDBInfo[0].enums
+         .map(
+           (en) => `enum ${en.name} {
+            ${en.values.map((env) => `${env} = '${env}',`).join('\n')}
+       }`,
+         )
+         .join('\n')}
+       `
+           : ''
+       }
+
+       ${
+         sourceDBInfo[0]?.tables?.length > 0
+           ? `
+       ${sourceDBInfo[0]?.tables
+         .map(
+           (t) => `
+       interface ${t.name.toLowerCase().replace(/(?:^|_)(.)/g, (_, chr) => chr.toUpperCase())} {
+           ${t.cols
+             .map((col) => {
+               return `${col.column_name} : ${PostgresDataTypesAndOrm[col.udt_name]?.ormType || col.udt_name};`
+             })
+             .join('\n')}
+       }
+       `,
+         )
+         .join('\n')}
+       `
+           : ''
+       }
+       `
+
+        resolve(doc)
       } catch (error) {
         reject(error)
       }
