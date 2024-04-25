024da2cb765be0ef7c72455c69cfdf8da2a456e2
pradip <pradip.kharal777@gmail.com>
2024-04-26 02:24:56 +0545

    gen doc, gen interface

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import { IFile, Props } from './src/interface'
 import { ValidateDatabaseUrl, createFiles, readFileSync } from './src/utils'
 import { DB_SCOUT, DB_SCOUT_CONFIG_JSON } from './src/constants'
 import { PostgresqlClient } from './src/clients'
+import { DBSCOUT_SYSTEM_INFO_QUERY } from './src/queries'
 
 export class DbScout {
   private props!: Props
@@ -13,6 +14,7 @@ export class DbScout {
   constructor(props?: Props) {
     this.folderDir = path.join(process.cwd(), DB_SCOUT_CONFIG_JSON)
     this.readConfigSync(this.folderDir, props)
+    // this.updateDbScoutTableInfo()
   }
   private readConfigSync(configUrl: string, prp?: Props) {
     try {
@@ -38,7 +40,14 @@ export class DbScout {
   public getMigration(): Promise<string> {
     return new Promise(async (resolve, reject) => {
       try {
-        const migrationData = await new ScoutPostgresqlDb(this.props.sourceDbUrl).getMigrationWithOrder()
+        const migrationData = await new ScoutPostgresqlDb({
+          sourceDbUrl: this.props.sourceDbUrl,
+          destinationDbUrl: this.props.destinationDbUrl,
+        }).getMigrationWithOrder()
+        if (!fs.existsSync(this.folderDir)) {
+          fs.mkdirSync(this.folderDir, { recursive: true })
+        }
+        // const migrationDir = path.join(this.folderDir, `${Date.now()}`)
         if (!fs.existsSync(this.folderDir)) {
           fs.mkdirSync(this.folderDir, { recursive: true })
         }
@@ -67,13 +76,98 @@ export class DbScout {
     return new Promise(async (resolve, reject) => {
       try {
         if (this.props.destinationDbUrl && this.props.outputDirectory) {
+          const migrationquery = fs.readFileSync(`${this.props.outputDirectory}/migration.sql`, 'utf8')
+          if (!migrationquery) {
+            throw new Error('Migration not fount')
+          }
           const migration = await new PostgresqlClient(this.props.destinationDbUrl).runMigrationWithTransaction(
-            this.props.outputDirectory,
+            migrationquery,
           )
           resolve(migration)
           return
         }
         throw new Error('error occurred')
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+
+  public updateDbScoutTableInfo(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        if (!this.props.destinationDbUrl && !this.props.sourceDbUrl) {
+          throw new Error('source or destination db url not found')
+        }
+        ;[this.props.sourceDbUrl, this.props.destinationDbUrl].forEach(async (dbUrl) => {
+          if (dbUrl) {
+            try {
+              await new PostgresqlClient(dbUrl).runMigrationWithTransaction(DBSCOUT_SYSTEM_INFO_QUERY)
+            } catch (error) {
+              throw new Error(error)
+            }
+          }
+        })
+        resolve('success')
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  public genDoc(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await new ScoutPostgresqlDb({
+          sourceDbUrl: this.props.sourceDbUrl,
+          destinationDbUrl: this.props.destinationDbUrl,
+        }).getDatabaseDocumentation()
+
+        if (!fs.existsSync(this.folderDir)) {
+          fs.mkdirSync(this.folderDir, { recursive: true })
+        }
+        const docDir = path.join(this.folderDir, `documentation`)
+        if (!fs.existsSync(docDir)) {
+          fs.mkdirSync(docDir, { recursive: true })
+        }
+        const files: Array<IFile> = [
+          {
+            filePath: path.join(docDir, 'info.txt'),
+            data,
+          },
+        ]
+        if (files.length > 0) {
+          await createFiles(files)
+        }
+        resolve('success')
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  public genInterface(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await new ScoutPostgresqlDb({
+          sourceDbUrl: this.props.sourceDbUrl,
+          destinationDbUrl: this.props.destinationDbUrl,
+        }).getInterfaces()
+        if (!fs.existsSync(this.folderDir)) {
+          fs.mkdirSync(this.folderDir, { recursive: true })
+        }
+        const interfaceDir = path.join(this.folderDir, `interfaces`)
+        if (!fs.existsSync(interfaceDir)) {
+          fs.mkdirSync(interfaceDir, { recursive: true })
+        }
+        const files: Array<IFile> = [
+          {
+            filePath: path.join(interfaceDir, 'index.ts'),
+            data,
+          },
+        ]
+        if (files.length > 0) {
+          await createFiles(files)
+        }
+        resolve('success')
       } catch (error) {
         reject(error)
       }
